0f6cb2675124499b482a115c27bbdf34a4261eee
shahzaibkamal <161307227+shahzaibkamal@users.noreply.github.com>
2024-05-04 19:47:03 +0500

    video 10: Add slide to height min to max   url:https://www.youtube.com/watch?v=CSJI9YHYGMY&list=PLGJPpb8f27YY-gBqVlUbWNz3Eo5IplFlT&index=82
    video 10: Add slide to height min to max
    and colors logic drag to active to de active   url :  https://www.youtube.com/watch?v=CSJI9YHYGMY&list=PLGJPpb8f27YY-gBqVlUbWNz3Eo5IplFlT&index=82

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -6,7 +6,7 @@ import 'container_file.dart';
 import 'icon_file.dart';
 import 'constantFile.dart'; // Corrected import
 import 'resultFile.dart';
-import 'calculatorFile.dart';
+
 
 enum Gender {
   male,
@@ -30,88 +30,85 @@ class _InputPageState extends State<InputPage> {
         title: Text('BMI CALCULATOR'),
       ),
       body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+      Expanded(
+      child: Row(
+          children: <Widget>[
           Expanded(
-            child: Row(
-              children: <Widget>[
-                Expanded(
-                  child: ContainerRepeated(
-                    onPressed: () {
-                      setState(() {
-                        selectedGender = Gender.male;
-                      });
-                    },
-                    cardWidget: RepeatedIcon(
-                      color: selectedGender == Gender.male ? activeColor : deactiveColor,
-                      iconData: FontAwesomeIcons.male,
-                      label: 'Male',
-                    ),
-                    colors: selectedGender == Gender.male ? activeColor : deactiveColor,
-                  ),
-                ),
-                Expanded(
-                  child: ContainerRepeated(
-                    onPressed: () {
-                      setState(() {
-                        selectedGender = Gender.female;
-                      });
-                    },
-                    cardWidget: RepeatedIcon(
-                      color: selectedGender == Gender.female ? activeColor : deactiveColor,
-                      iconData: FontAwesomeIcons.female,
-                      label: 'Female',
-                    ),
-                    colors: selectedGender == Gender.female ? activeColor : deactiveColor,
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Expanded(
-            child: ContainerRepeated(
-              colors: Color(0xFF1D1E33),
-              cardWidget: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    'Height',
-                    style: kLabelStyle,
-                  ),
-                  Row(
+          child: ContainerRepeated(
+          onPressed: () {
+    setState(() {
+    selectedGender = Gender.male;
+    });
+    },
+      cardWidget: RepeatedIcon(
+        color: selectedGender == Gender.male ? activeColor : deactiveColor,
+        iconData: FontAwesomeIcons.male,
+        label: 'Male',
+      ),
+      colors: selectedGender == Gender.male ? activeColor : deactiveColor,
+    ),
+    ),
+    Expanded(
+    child: ContainerRepeated(
+    onPressed: () {
+    setState(() {
+    selectedGender = Gender.female;
+    });
+    },
+    cardWidget: RepeatedIcon(
+    color: selectedGender == Gender.female ? activeColor : deactiveColor,
+    iconData: FontAwesomeIcons.female,
+    label: 'Female',
+    ),
+    colors: selectedGender == Gender.female ? activeColor : deactiveColor,
+    ),
+    ),
+    ],
+    ),
+    ),
+    Expanded(
+    child: ContainerRepeated(
+    colors: Color(0xFF1D1E33),
+    cardWidget: Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: <Widget>[
+    Text(
+    'Height',
+    style: kLabelStyle,
+    ),
+    Row(
 
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text(
-                        sliderHeight.toString(),
-                        style: TextStyle(
-                          fontSize: 35.0,
-                          fontWeight: FontWeight.w900,
-                        ),
-                      ),
-                      Text(
-                        'cm',
-                        style: kLabelStyle,
-                      ),
-                    ],
-                  ),
-                  Slider(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: <Widget>[
+    Text(
+    sliderHeight.toString(),
+    style: TextStyle(
+    fontSize: 35.0,
+    fontWeight: FontWeight.w900,
+    ),
+    ),
+    Text(
+    'cm',
+    style: kLabelStyle,
+    ),
+    ],
+    ),
+    Slider(
 
-                    value: sliderHeight.toDouble(),
-                    min: 120.0,
-                    max: 220.0,
-                    activeColor: Color(0xFFEB1555),
-                    inactiveColor: Color(0xFF8D8E98),
-                    onChanged: (double newValue) {
-                      setState(() {
-                        sliderHeight = newValue.round();
-                      });
-                    },
-                  ),
-                ],
-              ), onPressed: () {  },
-            ),
-          ),
-
-
-
+    value: sliderHeight.toDouble(),
+    min: 120.0,
+    max: 220.0,
+    activeColor: Color(0xFFEB1555),
+    inactiveColor: Color(0xFF8D8E98),
+    onChanged: (double newValue) {
+    setState(() {
+    sliderHeight = newValue.round();
+    });
+    },
+    ),
+    ],
+    ), onPressed: () {  },
+    ),
+    ),
